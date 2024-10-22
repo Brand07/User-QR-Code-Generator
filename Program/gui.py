662ad3b4a7f7125914b932be7f2a46374f24a7eb
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-10-22 10:56:28 -0500

    Add base gui

--- Program/gui.py
+++ Program/gui.py
@@ -4,3 +4,15 @@ import qrcode
 
 customtkinter.set_appearance_mode("light")
 
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("GSN - QR Code Generator")
+        self.geometry("600x400")
+        self.resizable(False, False)
+
+
+
+app = App()
+app.mainloop()
