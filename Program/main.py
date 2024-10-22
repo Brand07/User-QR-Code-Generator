00d51436f692fd7e9ab00da7a17829544d059927
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-10-22 11:18:40 -0500

    Add function to generate QR codes

--- Program/main.py
+++ Program/main.py
@@ -16,8 +16,18 @@ class App(customtkinter.CTk):
         self.user_entry.pack(pady=20)
         self.password_entry = Entry(self, placeholder_text="Password")
         self.password_entry.pack(pady=20)
-        self.generate_button = Button(self, text="Generate QR Code")
+        self.generate_button = Button(self, text="Generate QR Code", command=self.generate_qr_code)
         self.generate_button.pack(pady=20)
+
+    def generate_qr_code(self):
+        user_text = self.user_entry.get()
+        password_text = self.password_entry.get()
+        qr_data = f"{user_text}\t{password_text}\t\t\t"
+        qr = qrcode.QRCode(version=1, box_size=10, border=5)
+        qr.add_data(qr_data)
+        qr.make(fit=True)
+        img = qr.make_image(fill='black', back_color='white')
+        img.save("qrcode.png")
 
 
 
