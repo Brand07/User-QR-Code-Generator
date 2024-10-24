4e366833736499f6e7bb1e34c72d9624587db87b
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-10-24 08:28:48 -0500

    Add ability to choose if you want to print the QR code or not.

--- Program/main.py
+++ Program/main.py
@@ -13,16 +13,20 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.title("GSN - QR Code Generator v0.1")
-        self.geometry("600x450")
+        self.geometry("600x500")
         self.resizable(False, False)
         self.user_entry = Entry(self, placeholder_text="Username")
         self.user_entry.pack(pady=20)
         self.password_entry = Entry(self, placeholder_text="Password", show="*")
         self.password_entry.pack(pady=20)
+        self.print_option = customtkinter.CTkCheckBox(self, text="Print QR Code")
+        self.print_option.pack(pady=20)
         self.generate_button = Button(self, text="Generate QR Code", command=self.generate_qr_code)
         self.generate_button.pack(pady=20)
         self.label_width = 150
         self.label_height = 150
+
+
 
         #Display the QR Code Image
         self.qr_label = customtkinter.CTkLabel(self, text="")
@@ -37,19 +41,30 @@ class App(customtkinter.CTk):
         password_text = self.password_entry.get()
         if password_text == "":
             print("Password is required.")
-        qr_data = f"{user_text}\t{password_text}\t\t\t"
+        qr_data = f"{user_text}\t{password_text}\t\t\t\t"
         qr = qrcode.QRCode(version=1, box_size=10, border=5)
         qr.add_data(qr_data)
         qr.make(fit=True)
         img = qr.make_image(fill='black', back_color='white')
         img.save(f"QR_Codes/{user_text}.png")
         self.clear_entries()
-        # Send the generated QR Code to the printer
-        self.print_image(f"QR_Codes/{user_text}.png", self.label_width, self.label_height)
-        img_tk = ImageTk.PhotoImage(img)
-        #Display the QR Code in the GUI
-        self.qr_label.configure(image=img_tk)
-        self.qr_label.image = img_tk
+
+        if self.print_option.get() == 1:
+            self.print_image(f"QR_Codes/{user_text}.png", self.label_width, self.label_height)
+            img_tk = ImageTk.PhotoImage(img)
+            # Display the QR Code in the GUI
+            self.qr_label.configure(image=img_tk)
+            self.qr_label.image = img_tk
+        else:
+            print("QR Code generated but not printed.")
+            # Display the QR Code in the GUI
+            img_tk = ImageTk.PhotoImage(img)
+            self.qr_label.configure(image=img_tk)
+            self.qr_label.image = img_tk
+
+
+
+
     def clear_entries(self):
         """Clears the username and password entry fields"""
         self.user_entry.delete(0, "end")
